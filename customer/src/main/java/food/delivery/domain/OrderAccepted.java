6d1eaa6147d549a503823705c0edf6a5ee5c19fe
ycgee <youngsteel@gmail.com>
2022-11-21 11:22:53 +0900

    commit

---
+++ customer/src/main/java/food/delivery/domain/OrderAccepted.java
@@ -0,0 +1,19 @@
+package food.delivery.domain;
+
+import food.delivery.domain.*;
+import food.delivery.infra.AbstractEvent;
+import lombok.*;
+import java.util.*;
+@Data
+@ToString
+public class OrderAccepted extends AbstractEvent {
+
+    private Long id;
+    private String status;
+    private String foodId;
+    private String customerId;
+    private String storeId;
+    private Object options;
+}
+
+
